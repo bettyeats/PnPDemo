24002ca10c537aa31ae41e1cf8969b725ef79dc0
ting.yang <ting.yang@remarkdip.com>
2018-02-27 15:10:10 +0800

    Initial Commit

---
+++ Facial_Features/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Facial_Features
+//
+//  Created by betty on 2018/2/27.
+//  Copyright © 2018年 betty. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
